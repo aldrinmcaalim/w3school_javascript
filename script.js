60f4685cfa5f03716620d4aa7de3ec218e71bca6
Aldrin <aldrinmcaalim@gmail.com>
2021-02-23 12:44:12 -0500

    Used document.write()
    Used best for testing purposes. WARNING, using it after an HTML document is loaded will replace the entire HTML document.

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function remove_item() {
 function return_item() {
     document.getElementById("animal_noise").style.display = 'block';
 }
+
+function ten_plus_ten() {
+    document.getElementById("answer").innerHTML = `Answer: ${10 + 10}`;
+}
+
+function change_entire_html() {
+    document.write(1 + 1);
+}
