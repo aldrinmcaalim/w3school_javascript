fd5d38c4e330ca085a7d72ca647c11c787a64f9b
Aldrin <aldrinmcaalim@gmail.com>
2021-02-23 10:31:09 -0500

    Used functions on buttons
    Allows the code to be reusable and easily debugged

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("animal_noise").innerHTML = "I'm in a seperate JavaScript File!";
+}
+
+function remove_item() {
+    document.getElementById("animal_noise").style.display = 'none';
+}
+
+function return_item() {
+    document.getElementById("animal_noise").style.display = 'block';
+}
